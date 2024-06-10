c49561a845cc9fe842e623b4b154de89173da18f
DerputeR <dekxy-developer@yahoo.com>
2024-06-10 10:58:00 -0400

    Added dynamic_programming folder and placed lis.cpp there; Started lcs.pp and lcss.cpp

---
+++ dynamic_programming/lcs.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include "common.h"
+
+// Longest Common Subsequence (LCS)
+// Given two strings, the goal is to figure out the length of the LCS
+// shared by these strings, then return the actual LCS
+
+int main()
+{
+
+}
---
+++ dynamic_programming/lcss.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include "common.h"
+
+// Longest Common SubString (LCSS)
+// Given two strings, the goal is to figure out the length of the LCSS
+// shared by these strings, then return the actual LCSS
+
+int main()
+{
+
+}
