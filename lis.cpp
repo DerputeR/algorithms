c4e51ddee2d3a12f485eeb04289b6bdb9938bb6d
DerputeR <dekxy-developer@yahoo.com>
2024-06-09 22:15:23 -0400

    Added complete non-templated version for ease of reference

--- lis.cpp
+++ lis.cpp
@@ -41,33 +41,34 @@ std::vector<int> lis_lengths(const std::vector<a> &A)
 template <typename a>
 std::vector<a> lis(const std::vector<a> &A, const std::vector<a>* lengths_ptr = nullptr)
 {
-    std::vector<int> lengths;
+    std::vector<int> L;
     if (lengths_ptr == nullptr)
     {
-        lengths = lis_lengths(A);
+        L = lis_lengths(A);
     }
     else
     {
-        lengths = *lengths_ptr;
+        L = *lengths_ptr;
     }
 
+    // determine the index of the longest length in L
     int max_index = 0;
-    for (int i = 1; i < lengths.size(); i++)
+    for (int i = 1; i < L.size(); i++)
     {
-        if (lengths[i] > lengths[max_index])
+        if (L[i] > L[max_index])
         {
             max_index = i;
         }
     }
 
-    std::vector<a> lis_vec(lengths[max_index], 0);
+    // using max_index, trace backwards to generate the LIS
+    std::vector<a> lis_vec(L[max_index], 0);
     size_t vec_index = lis_vec.size() - 1;
     lis_vec[vec_index] = A[max_index];
     vec_index--;
-
     for (int j = max_index - 1; j >= 0; j--)
     {
-        if (lengths[j] == lengths[max_index] - 1)
+        if (L[j] == L[max_index] - 1)
         {
             max_index = j;
             lis_vec[vec_index] = A[j];
@@ -78,12 +79,50 @@ std::vector<a> lis(const std::vector<a> &A, const std::vector<a>* lengths_ptr = 
     return lis_vec;
 }
 
+// non-templated version
+std::vector<int> lis(const std::vector<int> &A) {
+    // create list for lengths of LIS ending with a_i
+    std::vector<int> L(A.size(), 1);
+    for (int i = 1; i < A.size(); i++) {
+        for (int j = i - 1; j >= 0; j--) {
+            if (A[j] < A[i] && L[i] < 1 + L[j]) {
+                // L[i] is 1 + the length of the LIS
+                // that can have A[i] appended to it
+                L[i] = 1 + L[j];
+            }
+        }
+    }
+    // determine the index of the longest length in L
+    int max_index = 0;
+    for (int i = 1; i < L.size(); i++) {
+        if (L[i] > L[max_index]) {
+            max_index = i;
+        }
+    }
+    // using max_index, trace backwards to generate the LIS
+    std::vector<int> lis_vec(L[max_index], 0);
+    size_t vec_index = lis_vec.size() - 1;
+    lis_vec[vec_index] = A[max_index];
+    vec_index--;
+    for (int j = max_index - 1; j >= 0; j--) {
+        if (L[j] == L[max_index] - 1) {
+            max_index = j;
+            lis_vec[vec_index] = A[j];
+            vec_index--;
+        }
+    }
+    return lis_vec;
+}
+
 int main()
 {
     std::vector<int> input{5, 7, 4, -3, 9, 1, 10, 4, 5, 8, 9, 3};
     std::vector<int> lengths = lis_lengths(input);
     std::vector<int> output = lis(input, &lengths);
+    // std::vector<int> output = lis(input);
 
+    std::cout << "Lengths: ";
     print_vec(lengths);
+    std::cout << "LIS: ";
     print_vec(output);
 }
